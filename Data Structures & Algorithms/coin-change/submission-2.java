b33b43d5c697628b7d07ff4ce9aedb6ee40535a0
abhi1289-gif <abhisheksonparote6@gmail.com>
2026-05-23 11:12:39 +0530

    Add: coin-change - submission-2

---
+++ Data Structures & Algorithms/coin-change/submission-2.java
@@ -0,0 +1,35 @@
+class Solution {
+
+    int INF = (int)1e9;
+
+    public int findWays(int[] coins, int amount, int sum, int dp[]){
+        if(sum == amount) return 0;
+        if(sum > amount) return INF;
+
+        if(dp[sum] != -1) return dp[sum];
+
+        int ans = INF;
+
+        for(int coin : coins){
+
+            if(sum > amount - coin)
+                continue;
+
+            int res =
+            findWays(coins, amount, sum + coin, dp);
+
+            if(res != INF)
+                ans = Math.min(ans,1+res);
+        }
+
+        return dp[sum] = ans;
+    }
+
+    public int coinChange(int[] coins, int amount) {
+        int dp[] = new int[amount+1];
+        for(int i=0; i<=amount; i++) dp[i] = -1;
+        int sum = 0;
+        int result = findWays(coins, amount, sum, dp);
+        return (result==INF)? -1: result;
+    }
+}
